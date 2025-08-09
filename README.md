da4ccacaa49d134452ea74055d283d7c2b15f94e
Diego Lizarazo <hielo777@gmail.com>
2025-08-09 00:15:30 -0400

    Improving UX

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 This project showcases how to create a simple and effective thumbstick controller for 3D objects in Construct 3.
 
 With 3 events, and some clever setting, the game can set the thumbstick controller, that can be used in any touch-screen device.
+
+## Changes:
+
+- Better Rotation of the 3D Object
+I left 2 3D objects so the difference between the way to handle their rotation looks like.
+In the next version I will eliminate the second object, and work on collisions, that way this would be the base of a 3D type of game.
+
+- Re created the exported game files
--- scripts/objRefTable.js
+++ scripts/objRefTable.js
@@ -8,18 +8,21 @@ self.C3_GetObjectRefTable = function () {
 		C3.Behaviors.DragnDrop,
 		C3.Plugins.Tilemap,
 		C3.Plugins.Touch,
+		C3.Behaviors.Pin,
 		C3.Plugins.System.Cnds.EveryTick,
 		C3.Plugins.Camera3D.Acts.LookAtPosition,
-		C3.Plugins.Shape3D.Exps.X,
-		C3.Plugins.Shape3D.Exps.Y,
-		C3.Behaviors.DragnDrop.Cnds.IsDragging,
-		C3.Plugins.Sprite.Cnds.PickDistance,
 		C3.Plugins.Sprite.Exps.X,
 		C3.Plugins.Sprite.Exps.Y,
+		C3.Behaviors.DragnDrop.Cnds.IsDragging,
+		C3.Plugins.Sprite.Cnds.PickDistance,
 		C3.Behaviors.EightDir.Acts.SetVectorX,
 		C3.Behaviors.EightDir.Acts.SetVectorY,
+		C3.Plugins.Shape3D.Acts.RotateTowardAngle,
+		C3.Plugins.Sprite.Exps.Angle,
 		C3.Behaviors.DragnDrop.Cnds.OnDrop,
-		C3.Plugins.Sprite.Acts.SetPos
+		C3.Plugins.Sprite.Acts.SetPos,
+		C3.Plugins.System.Cnds.OnLayoutStart,
+		C3.Behaviors.Pin.Acts.PinByProperties
 	];
 };
 self.C3_JsPropNameTable = [
@@ -34,6 +37,9 @@ self.C3_JsPropNameTable = [
 	{directionMarker: 0},
 	{Tilemap: 0},
 	{Touch: 0},
+	{playerBase: 0},
+	{Pin: 0},
+	{"3DShape": 0},
 	{cameraHeight: 0},
 	{speedPlayer: 0}
 ];
@@ -45,5 +51,7 @@ self.InstanceType = {
 	thumbstick01: class extends self.ISpriteInstance {},
 	directionMarker: class extends self.ISpriteInstance {},
 	Tilemap: class extends self.ITilemapInstance {},
-	Touch: class extends self.IInstance {}
+	Touch: class extends self.IInstance {},
+	playerBase: class extends self.ISpriteInstance {},
+	_3DShape: class extends self.I3DShapeInstance {}
 }
